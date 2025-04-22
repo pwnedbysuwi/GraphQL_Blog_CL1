810be2622375b393aab3be5a8304c4a9d3d06f66
purple.haz3 <94627848+pwnedbysuwi@users.noreply.github.com>
2025-04-22 21:39:38 +0800

    Update Author.jsx

--- components/Author.jsx
+++ components/Author.jsx
@@ -3,22 +3,28 @@ import Image from 'next/image';
 
 import { grpahCMSImageLoader } from '../util';
 
-const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-    <div className="absolute left-0 right-0 -top-14">
-      <Image
-        unoptimized
-        loader={grpahCMSImageLoader}
-        alt={author.name}
-        height="100px"
-        width="100px"
-        className="align-middle rounded-full"
-        src={author.photo.url}
-      />
+const Author = ({ author }) => {
+  return (
+    <div className="relative mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20 text-center">
+      <div className="absolute -top-14 left-0 right-0 flex justify-center">
+        <Image
+          unoptimized
+          loader={grpahCMSImageLoader}
+          src={author.photo.url}
+          alt={`Photo of ${author.name}`}
+          width={100}
+          height={100}
+          className="rounded-full align-middle"
+        />
+      </div>
+      <h3 className="mt-4 mb-4 text-xl font-bold text-white">
+        {author.name}
+      </h3>
+      <p className="text-white text-ls">
+        {author.bio}
+      </p>
     </div>
-    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
-    <p className="text-white text-ls">{author.bio}</p>
-  </div>
-);
+  );
+};
 
 export default Author;
